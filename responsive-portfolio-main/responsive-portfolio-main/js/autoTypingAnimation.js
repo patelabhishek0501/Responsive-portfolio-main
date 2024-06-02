1dfae89cde10ee69d22740ea1d1e0599620b14d0
patelabhishek0501 <147438445+patelabhishek0501@users.noreply.github.com>
2024-06-02 14:50:59 +0530

    Add files via upload

---
+++ responsive-portfolio-main/responsive-portfolio-main/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Abhishek</span>  B.Tech student at Bansal Institute of Science and Technology, Bhopal. . Proficient in C, C++, HTML, CSS, JavaScript, SQL. Experienced in AI-driven Talk and Event Management web app development! <br><br>  A team player, self-motivated, adept problem solver. Achieved success in leading group projects."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
